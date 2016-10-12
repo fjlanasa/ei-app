365855e2113aa186686569b54f48a55f59fe0540
Frank <fjlanasa@gmail.com>
2016-10-11 22:52:59 -0400

    used env variables

--- app/controllers/api/level_one_words_controller.rb
+++ app/controllers/api/level_one_words_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 class Api::LevelOneWordsController < ApiController
   def index
     level_one_words = LevelOneWord.all
@@ -10,10 +13,10 @@ class Api::LevelOneWordsController < ApiController
     name = params[:name]
     url2 = "https://wordsapiv1.p.mashape.com/words/#{name}"
     str2 = ''
-    open(url2,"X-Mashape-Key" => "tWoEhzcNoImshNr61g8SmApOU9LBp1Llbr2jsn2vvnaJD0KZrj") {|f| f.each_line {|line| str2 += line}}
-    data2 = JSON.parse(str2)
+    open(url2,"X-Mashape-Key" => "#{ENV['WORD_KEY']}") {|f| f.each_line {|line| str2 += line}}
+    data2= JSON.parse(str2)
     definition = data2['results'][0]['definition']
-    url3 = "http://api.giphy.com/v1/gifs/search?q=#{name}&api_key=dc6zaTOxFJmzC"
+    url3 = "http://api.giphy.com/v1/gifs/search?q=#{name}&api_key=#{ENV['GIPHY_KEY']}"
     url3 = URI.parse(url3)
     str3 = url3.read
     data3 = JSON.parse(str3)
