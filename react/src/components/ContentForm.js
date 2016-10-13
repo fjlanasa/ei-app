8eb1d999c590fd079b212002dfd89b1dc3a60a1d
Frank <fjlanasa@gmail.com>
2016-10-13 19:17:21 -0400

    changed content form to textarea

--- react/src/components/ContentForm.js
+++ react/src/components/ContentForm.js
@@ -2,15 +2,17 @@ import React from 'react';
 
 const ContentForm = props =>{
   return (
-    <form onSubmit={props.handleFormSubmit} className = "row">
+    <form onSubmit={props.handleFormSubmit} className = "row" id="content-form">
         <div className="input-group">
-          <input
-            className="input-group-field"
-            type="text"
-            placeholder="Enter something"
+          <textarea
+            form="content-form"
+            className="textarea"
+            cols="5"
+            rows="20"
+            wrap="hard"
             value={props.form_text}
             onChange={props.handleChange}
-          />
+          >Enter Something...</textarea>
         </div>
         <div className="input-group-button">
             <input type="submit" className="button" value="Add To List" />
